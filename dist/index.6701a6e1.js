79078ec2bd40bdb20c19800815b7927aa8ac1ad9
zackkb <98679473+zackkb@users.noreply.github.com>
2022-05-04 00:22:07 -0500

    fixed ImagePath to ImageURL

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -25482,7 +25482,7 @@ class MovieView extends _reactDefault.default.Component {
                     },
                     __self: this,
                     children: /*#__PURE__*/ _jsxRuntime.jsx("img", {
-                        src: movie.ImagePath,
+                        src: movie.ImageURL,
                         __source: {
                             fileName: "src/components/movie-view/movie-view.jsx",
                             lineNumber: 11
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={movie.ImageURL} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
