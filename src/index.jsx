53765830e657641c01bbf7929972cbc3c58a2ee6
zackkb <98679473+zackkb@users.noreply.github.com>
2022-05-12 22:06:54 -0500

    Updated react to utilize new features

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
+import { MovieView } from "./components/movie-view/movie-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+import "./index.scss";
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <MainView />
-    );
+    return <MainView />;
   }
 }
 
 // Finds the root of your app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+createRoot(container).render(React.createElement(MyFlixApplication));
+
+//ReactDOM.render(React.createElement(MyFlixApplication), container);
