7a8b05879fecc8cbfdbc1e590a180fd819ed8d0d
zackkb <98679473+zackkb@users.noreply.github.com>
2022-05-17 23:25:01 -0500

    Updating the componentDidMount method to persist login data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -21,16 +21,13 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    axios
-      .get("https://zachmovie.herokuapp.com/movies")
-      .then((response) => {
-        this.setState({
-          movies: response.data,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
+    let accessToken = localStorage.getItem("token");
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem("user"),
       });
+      this.getMovies(accessToken);
+    }
   }
 
   setSelectedMovie(movie) {
